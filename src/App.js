6aa64bc651c3142e27f5d4ba6f29c924445f9f34
GRAviTY-GAGAN <68629598+GRAviTY-GAGAN@users.noreply.github.com>
2022-07-15 11:22:14 +0530

    update

--- src/App.js
+++ src/App.js
@@ -24,11 +24,14 @@ function App() {
 
       {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto">No Images Found</h1>}
 
-      {isLoading ? <h1 className="text-6xl text-center mx-auto">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
-        {images.map(image => (
+      {isLoading ? <h1 className="text-6xl text-center mx-auto">Loading...</h1>
+       : <div className="grid grid-cols-3 gap-4 grid2">
+          {images.map(image => 
+          (
           <ImageCard key={image.id} image={image}/>
-        ))}
-      </div>}
+          )
+          )}
+        </div>}
     </div>
   );
 }
